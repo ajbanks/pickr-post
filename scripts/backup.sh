df8fb6764781e25c0e5ae62ca2e34207edeb24bc
meiji163 <mysatellite99@gmail.com>
2023-09-17 19:11:09 -0700

    fix backup script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/bash
 
 source "/home/web/.env"
-export PGPASSWORD=$PGPASSWORD
 
 DATABASE="pickr"
 GCLOUD_BUCKET="gs://pickr-prod-postgres-backups"
 backup_name="${DATABASE}_$(date +'%Y-%m-%dT%H:%M')"
 
 echo -n "==> dumping postgres database ${DATABASE}..."
-pg_dump -U pickr_super pickr | zstd -7 > "/backups/${backup_name}.zst"
+PGPASSWORD=$PGPASSWORD pg_dump -U pickr_super pickr | zstd -7 > "/backups/${backup_name}.zst"
 echo "done"
 
 # keep backups for one week
-find /backups -delete -mtime +6 -iname
+find /backups -delete -mtime +6 -iname 'pickr.*zst'
 
 gcloud storage cp "/backups/$backup_name" $GCLOUD_BUCKET
